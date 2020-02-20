e21d40f132e103e1f1fc633b9b2eed66c615f7b3
Daniel <danielhoful@gmail.com>
2020-02-20 13:41:02 -0600

    Created Django project with PyCharm IDE

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\danie\Desktop\School semesters\Spring 2020\CS 451R\semester_project_group_13\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
