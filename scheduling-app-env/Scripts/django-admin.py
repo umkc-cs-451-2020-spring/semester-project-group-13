f8ed3bafd09bc2eb9ac9fe4c90dfed6404bdc9c8
sjanesko <sjjmt4@mail.umkc.edu>
2020-03-04 20:06:23 -0600

    fixed merge conflict

---
+++ scheduling-app-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\stanley\Documents\Programming Projects\Python Projects\semester-project-group-13\scheduling-app-env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
